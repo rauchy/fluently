9c1a6b27948aaeb8e9314447599d32a5dbdd6509
Omer Rauchwerger <rauchy@gmail.com>
2011-09-26 13:29:47 +0300

    added a spec for simple methods with parameters

--- spec/fluently_spec.rb
+++ spec/fluently_spec.rb
@@ -2,13 +2,25 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 describe "def_fluently" do
-  it "defines a simple instance method" do
-		instance = Class.new do 
-			def_fluently 'foo' do
-				"Bar!"
-			end
-		end.new
+	describe "for instance methods" do
+		it "defines a simple method" do
+			instance = Class.new do 
+				def_fluently 'foo' do
+					"Bar!"
+				end
+			end.new
 
-		instance.foo.must_equal "Bar!"
+			instance.foo.must_equal "Bar!"
+		end
+		
+		it "defines a method with parameters" do
+			instance = Class.new do 
+				def_fluently 'foo' do |name|
+					"Hello, #{name}!"
+				end
+			end.new
+
+			instance.foo("rauchy").must_equal "Hello, rauchy!"
+		end
 	end
 end	
